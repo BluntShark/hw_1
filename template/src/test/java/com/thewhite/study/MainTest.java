4d4cf6da5792956e15389eb87c2ddf109786c460
Anastasia <96139628+frabsis@users.noreply.github.com>
2023-10-26 11:50:55 +1000

    Add files via upload

--- template/src/test/java/com/thewhite/study/MainTest.java
+++ template/src/test/java/com/thewhite/study/MainTest.java
@@ -1,4 +1,6 @@
 package com.thewhite.study;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,51 +19,62 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 public class MainTest {
+    public void writeToFile(){
+        Map<Integer, ResourceInfo<String>> resourceMap = new HashMap<>();
+        resourceMap.put(1, new ResourceInfo<>(1,"name1", "description1", "link1"));
+        resourceMap.put(2, new ResourceInfo<>(2,"name2", "description2", "link2"));
+        resourceMap.put(3, new ResourceInfo<>(3,"name3", "description3", "link3"));
+        resourceMap.put(4, new ResourceInfo<>(4,"name4", "description4", "link4"));
+        resourceMap.put(5, new ResourceInfo<>(5,"name5", "description5", "link5"));
+        resourceMap.put(6, new ResourceInfo<>(6,"name6", "description6", "link6"));
+
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("projectFiles/dictionary.txt"))) {
+            for (Map.Entry<Integer, ResourceInfo<String>> entry : resourceMap.entrySet()) {
+                int id = entry.getKey();
+                ResourceInfo<String> values = entry.getValue();
+                String line = id + "," + values.getName() + "," + values.getDescription() + "," + values.getLink();
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("HashMap записана в файл " + "projectFiles/dictionary.txt");
+    }
     @Test
-    void testGetIDInf0(){
+    void testGetIDInf0() {
         DisplayToScreen displayToScreen = new DisplayToScreen();
 
-        Map<Integer, String[]> resourceMap = new HashMap<>();
-        resourceMap.put(1, new String[]{"Name1", "Description1", "Link1"});
-        resourceMap.put(2, new String[]{"Name2", "Description2", "Link2"});
+        Map<Integer, ResourceInfo<String>> resourceMap = new HashMap<>();
+        resourceMap.put(1, new ResourceInfo<>(1, "name1", "description1", "link1"));
+        resourceMap.put(2, new ResourceInfo<>(2, "name2", "description2", "link2"));
+        resourceMap.put(3, new ResourceInfo<>(3, "name3", "description3", "link3"));
 
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
-        displayToScreen.printID(resourceMap, 1);
-        String expectedOutput = "Найденная запись:\n1 Name1 Description1 Link1\n";
+
+        int idToPrint = 2;
+        displayToScreen.printID(resourceMap, idToPrint);
+
+        System.setOut(System.out);
+
+        String expectedOutput = "2, name2, description2, link2\n";
         assertEquals(expectedOutput, outContent.toString());
-
-        outContent.reset();
-        displayToScreen.printID(resourceMap, 3);
-        expectedOutput = "Запись с указанным идентификатором не найдена\n";
-        assertEquals(expectedOutput, outContent.toString());
-
     }
+
     @Test
-    void testFindInfo(){
+    void testFindInfo() {
 
     }
+
     @Test
-    void testWriteToFile(){
-        ResourceInfo resourceInfo = new ResourceInfo();
-        resourceInfo.writeToFile();
+    void testWriteToFile() {
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("projectFiles/testFile.txt"))) {
-            String line;
-            int lineNumber = 1;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split(",");
-                assertEquals(4, parts.length);
-                assertEquals(Integer.toString(lineNumber), parts[0]);
-                assertEquals("name" + lineNumber, parts[1]);
-                assertEquals("description" + lineNumber, parts[2]);
-                assertEquals("link" + lineNumber, parts[3]);
-                lineNumber++;
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
-
 
 }
+
+
+
